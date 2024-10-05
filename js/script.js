8af885d701551297327fd2e094e44898e7189784
James <162724220+JMcKaay@users.noreply.github.com>
2024-10-05 16:26:22 +0100

    Modal Updated
    Modal updates, added in template for contacting/contact information

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ squares.forEach((square, index) => {
 
 // Initialize square positions
 updateSquarePositions();
+
+// Modal 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
